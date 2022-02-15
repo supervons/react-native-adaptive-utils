7293f9366326caa9344d2a1ed5e1a4e64d107fba
jinglj0512 <330487464@qq.com>
2022-02-15 10:58:35 +0800

    Description character limit

--- common/validator.js
+++ common/validator.js
@@ -32,3 +32,28 @@ export function validEmail(mail) {
 export function validCardNo(cardNo) {
   return RegExp(/(^\d{15}$)|(^\d{18}$)|(^\d{17}(\d|X|x)$)/).test(cardNo);
 }
+
+/**
+ * Valid Chinese person card.
+ * @param CharacterLimit
+ * @returns {boolean}
+ */
+export function limitstr(val, strnum) {
+  let re = "";
+  let strleng = val.length;
+  let byteleng = val.replace(/[^x00-xff]/g, "**").length;
+  if (byteleng <= strnum) return val;
+  for (var i = 0, bytenum = 0; i < strleng; i++) {
+    var byte = val.charAt(i);
+    if (/[\x00-\xff]/.test(byte)) {
+      bytenum += 1;
+    } else {
+      bytenum += 2;
+    }
+    if (bytenum <= strnum) {
+      re += byte;
+    } else {
+      return re;
+    }
+  }
+}
